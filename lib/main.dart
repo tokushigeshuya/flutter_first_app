f62931f14386afda7bdcc2c92cbb2d87a9a13eaa
tokushigeshuya <narihare1130@gmail.com>
2023-11-30 17:30:44 +0900

    [update]ウィジェットの追加(Scaffold,center)

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,13 @@ import 'package:flutter/material.dart';
 
 // voidがreturn type mainに関しては特殊なfunctionで実行の必要がない
 void main() {
-  runApp(const MaterialApp(home: Text('Hello World!')));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ), // centerウィジェットで中央揃え（ツリー構造になる）
+      ), // 足場となるfunction(Scaffold)
+    ),
+  );
 }
