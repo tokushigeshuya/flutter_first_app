af662e4319661c9395af7b689bbd8554819dca87
tokushigeshuya <narihare1130@gmail.com>
2023-11-30 18:32:19 +0900

    [update]ウィジェットの追加_1(color,gradation)

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,26 @@ import 'package:flutter/material.dart';
 // voidがreturn type mainに関しては特殊なfunctionで実行の必要がない
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            // グラデーション
+            gradient: LinearGradient(
+              // 複数カラーの選択
+              colors: [
+                Color.fromARGB(255, 211, 203, 227),
+                Color.fromRGBO(175, 131, 250, 1),
+              ],
+              //グラデーションスタート位置
+              begin: Alignment.topLeft,
+              // 終了位置
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
         ), // centerウィジェットで中央揃え（ツリー構造になる）
       ), // 足場となるfunction(Scaffold)
     ),
