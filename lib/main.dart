15558db4ccff036e667c05abbfecfb917dfc29eb
tokushigeshuya <narihare1130@gmail.com>
2023-12-01 19:17:14 +0900

    [update]カスタムウィジェットの構築

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 
-// voidがreturn type mainに関しては特殊なfunctionで実行の必要がない
+// voidがreturn type mainに関しては特殊なfunctionで実行の必要がない※dartのルールでvoidは値を返してはいけない
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            // グラデーション
-            gradient: LinearGradient(
-              // 複数カラーの選択
-              colors: [
-                Color.fromARGB(255, 211, 203, 227),
-                Color.fromRGBO(175, 131, 250, 1),
-              ],
-              //グラデーションスタート位置
-              begin: Alignment.topLeft,
-              // 終了位置
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!',
-            style: TextStyle(
-              fontSize: 60,
-              color: Colors.blueGrey,
-            ),
-            ),
-          ),
-        ), // centerウィジェットで中央揃え（ツリー構造になる）
+        body: GradientContainer(), // centerウィジェットで中央揃え（ツリー構造になる）
       ), // 足場となるfunction(Scaffold)
     ),
   );
+}
+// クラスの継承(extends)
+class GradientContainer extends StatelessWidget {
+  @override
+  // 関数の戻り値の型
+  Widget build(context){
+    return Container(
+      decoration: const BoxDecoration(
+        // グラデーション
+        gradient: LinearGradient(
+          // 複数カラーの選択
+          colors: [
+            Color.fromARGB(255, 211, 203, 227),
+            Color.fromRGBO(175, 131, 250, 1),
+          ],
+          //グラデーションスタート位置
+          begin: Alignment.topLeft,
+          // 終了位置
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text('Hello World!',
+        style: TextStyle(
+          fontSize: 60,
+          color: Colors.blueGrey,
+        ),
+        ),
+      ),
+    );
+  }
 }
